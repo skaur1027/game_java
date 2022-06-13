943858119c8aba2f69bd88542817f4bc003d2fba
skaur1027 <malhotra.sandeepk@gmail.com>
2022-06-13 11:26:39 -0700

    second commit

--- Game.java
+++ Game.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 /** A number guessing game. */
 public class Game {
   public static void main(String[] args) {
     System.out.println("Welcome to the Guessing Game!");
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter your name:");
+    String name = input.nextLine();
+    System.out.println(name + ", I'm thinking of a number between 1 and 100.");
   }
 }
