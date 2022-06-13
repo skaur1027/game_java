fe937854296dad5122787795c0860c3b0037c19d
skaur1027 <malhotra.sandeepk@gmail.com>
2022-06-13 11:53:56 -0700

    added code for game

--- Game.java
+++ Game.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Random;
+
 
 /** A number guessing game. */
 public class Game {
@@ -10,5 +12,19 @@ public class Game {
     System.out.println("Enter your name:");
     String name = input.nextLine();
     System.out.println(name + ", I'm thinking of a number between 1 and 100.");
+
+    Random rand = new Random();
+    int number = rand.nextInt(101);
+    int count = 0;
+
+    // ask the user for a guess
+    // check the guess against random
+    // initiate a counter for every guess, and increment it
+    System.out.println("Take your guess.");
+    String guess = input.nextLine();
+    int guess_num = Integer.parseInt(guess);
+    count += 1;
+    System.out.println(count + " guesses");
+    // System.out.println(number);
   }
 }
